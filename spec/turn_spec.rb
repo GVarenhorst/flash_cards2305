4fd3dd1f83705aff8630ebc277e1ef9bdb376b12
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-05-09 16:53:51 -0500

    Add exists test

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,2 +1,14 @@
 require './lib/turn'
-require './lib.card'
+require './lib/card'
+
+RSpec.describe Turn do
+  it "exists" do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn).to be_a(Turn)
+    expect(turn.card).to eq(card)
+  end
+
+  xit "can check if a guess is correct" do
+
+end
